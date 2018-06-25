769f55d2f1b0818e5f1ad49221acfccd901c4cbd
Lauren Frazier <32853159+laurenfrazier@users.noreply.github.com>
2018-06-25 16:43:40 +0200

    Create InputSystemManager and set up callback for verb coin games

---
+++ Assets/AdventureGame/InputSystem/CharacterActionType.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityEngine.AdventureGame
+{
+    /// <summary>
+    /// The type of action. The user will define the name, but we will refer
+    /// to the opaque action type under the hood. Whenever possible, the actions
+    /// will be displayed in ascending order.
+    /// The max number of 10 actions is an arbitrary number.
+    /// </summary>
+    public enum CharacterActionType {
+        ACTION1,
+        ACTION2,
+        ACTION3,
+        ACTION4,
+        ACTION5,
+        ACTION6,
+        ACTION7,
+        ACTION8,
+        ACTION9,
+        ACTION10,
+        NONE,
+    }
+}
---
+++ Assets/AdventureGame/InputSystem/InputSystemManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace UnityEngine.AdventureGame
+{
+    /// <summary>
+    /// The type of the game. Currently supported options include VerbCoin and Sierra. 
+    /// </summary>
+    public enum AdventureGameType {
+        SIERRA, // User selects an action before selecting the object to perform it on
+        VERBCOIN, // User selects an object and then selects an action to perform on it
+    }
+
+    /// <summary>
+    /// Handles user selections of objects and menu actions
+    /// ex. Walk, Speak, Pick Up
+    /// </summary>
+    []
+    public sealed class InputSystemManager : MonoBehaviour
+    {
+#region Internal Classes
+        /// <summary>
+        /// An action that a character can take.
+        /// ex. Walk, Speak, Pick Up
+        /// (Defined inside InputSystemManager to allow easy editing of available options in Editor)
+        /// </summary>
+        [System.Serializable]
+        public class CharacterAction
+        {
+            public string actionName;
+            public string actionDescription;
+            public Sprite actionIcon;
+            public CharacterActionType actionType;
+        }
+#endregion
+
+#region Public Variables
+        // Static singleton
+        public static InputSystemManager Instance
+        {
+            get
+            {
+                if (instance == null){
+                    instance = new InputSystemManager();
+                }
+                return instance;
+            }
+        }
+
+        // The type of adventure game. This will mostly impact the way the user
+        // interacts with game object (ex. select object then action vs. select action then object)
+        public AdventureGameType adventureGameType;
+
+        // The potential actions that a user can take.
+        public CharacterAction[] characterActions;
+
+        // The currently selected action (ACTION1, ACTION2, ..., NONE)
+        [HideInInspector]
+        public CharacterActionType currentlySelectedActionType;
+        public delegate void ActionSelectionDelegate(CharacterActionType selectedType = CharacterActionType.NONE);
+#endregion
+
+#region Private Variables
+        private static InputSystemManager instance;
+#endregion
+
+#region Public Methods
+        public void BeginActionSelection(ActionSelectionDelegate actionSelectionDelegate) {
+            // Display menu here
+
+            // Call delegate with result of action selection
+            if(actionSelectionDelegate != null) {
+                actionSelectionDelegate();
+            }
+        }
+#endregion
+
+#region Private Methods
+        private InputSystemManager() {}
+
+        private void Awake () {
+            DontDestroyOnLoad(this);
+        }
+#endregion
+    }
+}
