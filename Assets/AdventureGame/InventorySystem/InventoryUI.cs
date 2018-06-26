3054002b7e55fd69b64cd7d27eaa30ca39db88ca
Branwen Evans <evans.branwen@unity3d.com>
2018-06-26 15:37:06 +0200

    added inventory ui

---
+++ Assets/AdventureGame/InventorySystem/InventoryUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityEngine.AdventureGame
+{
+    public class InventoryUI : MonoBehaviour
+    {
+        InventoryManager inventoryManager;
+        InventorySlot[] inventorySlots;
+
+        void Start()
+        {
+            inventoryManager = FindObjectOfType<InventoryManager>();
+            inventorySlots = GetComponentsInChildren<InventorySlot>();
+            Debug.Log(inventorySlots.Length);
+
+            for (int i = 0; i < InventoryManager.INVENTORY_SLOTS; i++)
+            {
+                inventorySlots[i].index = i;
+                if(inventoryManager.items[i] != null){
+                    inventorySlots[i].GetComponent<Image>().sprite = inventoryManager.items[i].sprite;
+                }
+            }
+        }
+
+    }
+}
