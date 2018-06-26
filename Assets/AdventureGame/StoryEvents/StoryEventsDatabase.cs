a4501098c919e8a0824ff75691f3d000ec9f397e
Marija Boljanac <marijabo@nordeus.com>
2018-06-26 15:23:20 +0200

    Create story events scriptable object

---
+++ Assets/AdventureGame/StoryEvents/StoryEventsDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+
+public class StoryEventsDatabase : ScriptableObject
+{
+	public List<string> events;
+	
+	[UsedImplicitly]
+	[InitializeOnLoadMethod]
+	public static void CreateIfMissing()
+	{
+		EditorApplication.delayCall += () =>
+		{
+			var storyEventDatabasePath = "Assets/AdventureGame/StoryEvents/StoryEventsDatabase.asset";
+			var storyEventDatabase = AssetDatabase.LoadAssetAtPath<StoryEventsDatabase>(storyEventDatabasePath);
+			if (storyEventDatabase == null)
+			{
+				storyEventDatabase = ScriptableObject.CreateInstance<StoryEventsDatabase>();
+				AssetDatabase.CreateAsset(storyEventDatabase, storyEventDatabasePath);
+				AssetDatabase.SaveAssets();
+			}
+		};
+	}
+}
