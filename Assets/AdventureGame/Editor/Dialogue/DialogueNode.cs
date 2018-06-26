f8251d04ec6ce74505c69aa3421ded61d752827b
mario <mario@unity3d.com>
2018-06-26 07:28:01 -0700

    Updated graph with associated strings for each input/output

--- Assets/AdventureGame/Editor/Dialogue/DialogueNode.cs
+++ Assets/AdventureGame/Editor/Dialogue/DialogueNode.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using UnityEditor.Experimental.UIElements.GraphView;
 using UnityEngine.Experimental.UIElements;
+using UnityEngine.Experimental.UIElements.StyleEnums;
+using UnityEngine.Experimental.UIElements.StyleSheets;
 
 namespace Unity.Adventuregame {
     public class DialogueNode : Node {
@@ -11,14 +14,31 @@ namespace Unity.Adventuregame {
 
         void addOutput()
         {
-            Port output = Port.Create<Edge>(Orientation.Horizontal, Direction.Output, Port.Capacity.Single, typeof(bool));
-            outputContainer.Add(output);
+            Port outputPort = Port.Create<Edge>(Orientation.Horizontal, Direction.Output, Port.Capacity.Single, typeof(string));
+            outputPort.portName = string.Empty;
+            outputContainer.Add(outputPort);
+
+            TextField outText = new TextField
+            {
+                multiline = true
+            };
+            outputContainer.Add(outText);
+            outText.PlaceBehind(outputPort);
+
         }
 
         void addInput()
         {
-            Port inputPort = Port.Create<Edge>(Orientation.Horizontal, Direction.Input, Port.Capacity.Single, typeof(bool));
+            Port inputPort = Port.Create<Edge>(Orientation.Horizontal, Direction.Input, Port.Capacity.Single, typeof(string));
+            inputPort.portName = string.Empty;
             inputContainer.Add(inputPort);
+
+            TextField inText = new TextField
+            {
+                multiline = true
+            };
+            inputContainer.Add(inText);
+            inText.PlaceBehind(inputPort);
         }
 
         public override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
