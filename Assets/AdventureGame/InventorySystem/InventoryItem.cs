f62607931381f645cf7f85e402c490b325bbec34
Branwen Evans <evans.branwen@unity3d.com>
2018-06-27 16:17:02 +0200

    recreating lost inventory changes

--- Assets/AdventureGame/InventorySystem/InventoryItem.cs
+++ Assets/AdventureGame/InventorySystem/InventoryItem.cs
@@ -2,30 +2,34 @@
 
 namespace UnityEngine.AdventureGame
 {
-    public class InventoryItem : MonoBehaviour
-    {
-        public string Id;
-        public Sprite sprite;
-        SpriteRenderer spriteRenderer;
-        InventoryManager inventoryManager;
+	public class InventoryItem : MonoBehaviour
+	{
+		public string Id;
+		public Sprite sprite;
 
         void Start()
         {
-            inventoryManager = FindObjectOfType<InventoryManager>();
-            spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-            spriteRenderer.sprite = sprite;
+            SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            if (spriteRenderer)
+            {
+                //if the item has a sprite renderer make sure it's using the correct sprite
+                spriteRenderer.sprite = sprite;
+            }
         }
 
-        public void PickedUp(){
-            spriteRenderer.enabled = false;
-            inventoryManager.AddItem(this);
-        }
+		public void PickedUp()
+		{
+			Debug.Log(Id + " picked up.");
+			gameObject.SetActive(false); //disable the game object while it's in the inventory, so it doesn't appear in the scene
+			InventoryManager.Instance.AddItem(this);
+		}
 
 		public void Dropped()
 		{
-            spriteRenderer.enabled = true;
-			inventoryManager.RemoveItem(this);
+			Debug.Log(Id + " dropped.");
+			gameObject.SetActive(true); //enable the game object so it appears in the scene
+			InventoryManager.Instance.RemoveItem(this);
 		}
-    }
+	}
 }
 
