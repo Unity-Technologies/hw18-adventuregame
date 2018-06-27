cfb1bbb7460528800e02279210c0348fd81702c1
Ted Hung <tedh@unity3d.com>
2018-06-27 02:57:05 -0700

    Updating GameLogic editor.

---
+++ Assets/AdventureGame/GameLogic/GameLogicData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngine.AdventureGame
+{
+    [CreateAssetMenu(fileName = "GameLogic", menuName = "GameLogic", order = 1)]
+    public class GameLogicData : ScriptableObject
+    {
+        [Serializable]
+        public class GameLogicGraphNode
+        {
+            public string m_title;
+            public Vector2 m_position;
+            public List<GameLogicGraphEdge> m_outputs;
+        }
+
+        [Serializable]
+        public class GameLogicGraphEdge
+        {
+            public int m_sourcePort;
+            public int m_targetNode;
+            public int m_targetPort;
+        }
+
+        public List<GameLogicGraphNode> m_graphNodes = new List<GameLogicGraphNode>();
+    }
+}
