c6c68eb996aed54df14ed0bb40b8897010601f30
Ted Hung <tedh@unity3d.com>
2018-06-28 06:28:34 -0700

    Adding AnimTrigger and SetAnimBoolean renaming.

---
+++ Assets/AdventureGame/GameLogic/AnimTriggerNode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using UnityEditor.Experimental.UIElements.GraphView;
+using UnityEngine.Experimental.UIElements;
+
+namespace UnityEngine.AdventureGame
+{
+    public static class AnimTriggerNode
+    {
+        public static IEnumerator Execute(GameLogicData.GameLogicGraphNode currentNode)
+        {
+            SceneManager.Instance.Character.Animator.SetTrigger(currentNode.m_typeData);
+            yield return currentNode.GetReturnValue(0);
+        }
+
+#if UNITY_EDITOR
+        public static Node CreateNode(string typeData)
+        {
+            Node node = new Node();
+            node.title = "Anim Trigger";
+
+	        node.mainContainer.style.backgroundColor = Color.blue;
+
+			node.capabilities |= Capabilities.Movable;
+            Port inputPort = Port.Create<Edge>(Orientation.Horizontal, Direction.Input, Port.Capacity.Single, typeof(bool));
+            inputPort.portName = "";
+            inputPort.userData = null;
+            node.inputContainer.Add(inputPort);
+
+            var animParameterTextField = new TextField()
+            {
+                multiline = false,
+                value = typeData
+            };
+            node.mainContainer.Insert(1, animParameterTextField);
+
+            return node;
+        }
+
+        public static string ExtractExtraData(Node node)
+        {
+            foreach (VisualElement ele in node.mainContainer)
+            {
+                if (ele is TextField)
+                {
+                    return (ele as TextField).value;
+                }
+            }
+
+            return null;
+        }
+#endif
+    }
+}
