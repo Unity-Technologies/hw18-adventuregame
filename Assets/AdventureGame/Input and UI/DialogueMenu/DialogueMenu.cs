0af2cac571bbec02b39f906ea547f4b0bec7a36a
Lauren Frazier <32853159+laurenfrazier@users.noreply.github.com>
2018-06-28 15:28:59 +0200

    WIP: adding prefab dialogue boxes

--- Assets/AdventureGame/Input and UI/DialogueMenu/DialogueMenu.cs
+++ Assets/AdventureGame/Input and UI/DialogueMenu/DialogueMenu.cs
@@ -6,6 +6,16 @@ namespace UnityEngine.AdventureGame
 {
     public class DialogueMenu : MonoBehaviour
     {
-		
+		public void AddTitle(string title) {
+			Debug.Log("adding title:" + title);
+		}
+
+		public void AddDescription(string description) {
+			Debug.Log("adding description:" + description);
+		}
+
+		public void AddButton(string buttonTitle) {
+			Debug.Log("adding button:" + buttonTitle);
+		}
     }
 }
